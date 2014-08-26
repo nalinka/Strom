3d9437d66d52ccbd945d8ecaaa5a458b6300be55
nalinka <natalia.tokarova@gmail.com>
2014-08-26 21:12:08 +0200

    segmentation fault

--- strom.cpp
+++ strom.cpp
@@ -16,22 +16,40 @@ void PrintVector(const vector<string> list) {
 
 int main() {
 
-char a[200];
-vector<string> words;
-FILE* file = fopen("in.txt", "r");
-while (true) {
- int ret = fscanf(file, "%s", a);
- if (ret == EOF)
-  break;
- std::locale loc;
- std::string str = a;
- for (std::string::size_type i=0; i<str.length(); ++i){
-   str[i] = std::tolower(str[i],loc);
-   if (str[i] == '.' || str[i] == ',' || str[i] == '?' || str[i] == '!')
-     str.erase(i);
- }
- words.push_back(str);
-}
-PrintVector(words);
-fclose(file);
+  char a[200];
+  vector<string> words;
+  FILE* file = fopen("in.txt", "r");
+  while (true) {
+   int ret = fscanf(file, "%s", a);
+   if (ret == EOF)
+    break;
+   locale loc;
+   string str = a;
+   for (string::size_type i=0; i<str.length(); ++i){
+     str[i] = tolower(str[i],loc);
+     if (str[i] == '.' || str[i] == ',' || str[i] == '?' || str[i] == '!')
+       str.erase(i);
+   }
+   words.push_back(str);
+  }
+  int n = 0;
+  vector<int> pocet;
+  vector<string> nove;
+  sort(words.begin(), words.end());
+  
+  
+  for(int j =0; j < words.size(); j++){
+   if(words[j] == words[j+1]){
+    pocet[j]= pocet[j] + 1;
+   } else{
+    nove.push_back(words[j]);
+    n++;
+   } 
+  }
+  for(int i = 0; i < nove.size(); i++){
+    printf("%s %d \n",nove[i].c_str(), pocet[i]);
+  }
+  
+  
+  fclose(file);
 }
