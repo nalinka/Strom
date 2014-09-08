6960ca562a53545bfc13f8e7aaa2516bbfc7cfe1
nalinka <natalia.tokarova@gmail.com>
2014-09-08 16:29:41 +0200

    najcastejsie slovo

--- strom.cpp
+++ strom.cpp
@@ -12,6 +12,7 @@ int main() {
   char a[20000];
   vector<string> words;
   FILE* file = fopen("in.txt", "r");
+  //vlozit slova a normalizovat
   while (true) {
    int ret = fscanf(file, "%s", a);
    if (ret == EOF)
@@ -27,6 +28,7 @@ int main() {
    }
    words.push_back(str);
   }
+  //usporiadat a spocitat
   vector<int> pocet;
   vector<string> nove;
   sort(words.begin(), words.end());
@@ -46,10 +48,31 @@ int main() {
     }
   }
  }
-  for(int i = 0; i < nove.size(); i++){
-   printf("%s %d \n",nove[i].c_str(), pocet[i]);
-  }
+ //vypisat
+ for(int i = 0; i < nove.size(); i++){
+  printf("%s %d \n",nove[i].c_str(), pocet[i]);
+ }
+ char in[200];
+ scanf("%s", in);
+ string in_str = in;
+ bool n = false;
+ string slovo;
+ int max = 0;
+ for(int i = 0; i < nove.size(); i++){
+   for (int j=0; j<in_str.length(); ++j){
+     if(nove[i][j]==in_str[j]){
+       continue; 
+     }else
+       n == true;
+       break;
+   }
+   if(n == true)
+    break;
+   if(pocet[i] > max){
+     max = pocet[i];
+     slovo = nove[i];} 
+ }
+ printf("%s %d \n", slovo.c_str(), max); 
   
-  
-  fclose(file);
+ fclose(file);
 }
